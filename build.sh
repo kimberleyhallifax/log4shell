cfcab757a949ff52418ef2ff74d6e9b1a89f80cc
kimberleyhallifax <kimberley.hallifax@vgw.co>
2022-08-05 14:57:37 +0800

    Modified build script to cater to docker-compose file

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
-cd CVE-2021-44228-Apache-Log4j-Rce/
+cd containers/CVE/
 javac Exploit.java
 mvn clean package
-docker build -t server . # Build python server docker image
-cd ../marshalsec/
-docker build -t japp . # Build ldap server docker image
 cd ..
+docker-compose build
